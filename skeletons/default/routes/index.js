456ee976091b451827e5483a515afe4709ec6e11
sunkeysun <sunkeysun@tencent.com>
2017-12-08 20:46:45 +0800

    框架代码提交

---
+++ skeletons/default/routes/index.js
@@ -0,0 +1,11 @@
+/**
+ * 路由配置入口
+ *
+ * @author : sunkeysun
+ */
+
+import web from './web'
+
+export default (router) => {
+    router.group('/', web(router))
+}
---
+++ skeletons/default/routes/web/test.js
@@ -0,0 +1,5 @@
+
+
+export default (router) => {
+    router.get('/test', 'TestController@test')
+}
